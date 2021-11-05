00a5d3432c3e7bdb1d7a3376c4d2b4ab262f22aa
Stanislav Kalashnikov <stas@halega.com>
2021-11-05 07:25:09 +0300

    add missing semicolons

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 // Example from https://anyonecanlearntocode.com/think-like-a-software-engineer/videos/128
 
 function doubleValuesInArray(array) {
-    return array.map((n => n * 2))
+    return array.map((n => n * 2));
 }
 
-console.log(doubleValuesInArray([1, 2, 3, 4, 5]))
+console.log(doubleValuesInArray([1, 2, 3, 4, 5]));
