fc670a08feceddcc1d28c2effd1cc3efcbc1bcff
Stanislav Kalashnikov <stas@halega.com>
2021-11-09 22:54:59 +0300

    for

---
+++ for.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 12; i += 2) {
+    console.log(i);
+}
+// console.log(i); // i is not defined
+
+let result = 1;
+for (let i = 0; i < 10; i++) {
+    result *= 2;
+}
+console.log(result);
