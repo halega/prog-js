efd7e2667fe0000cc49cea637f514ae476e6f3f0
Stanislav Kalashnikov <stas@halega.com>
2021-11-09 23:29:21 +0300

    Ch02 exercise: author's solution to FizzBuzz

--- ch02-ex-fizzbuzz.js
+++ ch02-ex-fizzbuzz.js
@@ -9,3 +9,11 @@ for (let n = 1; n <= 100; n++) {
         console.log(n);
     }
 }
+
+// Solution from the author
+for (let n = 1; n <= 100; n++) {
+    let output = "";
+    if (n % 3 == 0) output += "Fizz";
+    if (n % 5 == 0) output += "Buzz";
+    console.log(output || n);
+}
