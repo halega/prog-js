9d31ce095664bf4b0042ac39554a58e948bda56e
Stanislav Kalashnikov <halega@gmail.com>
2025-03-18 23:20:57 +0300

    ch03: closure examples

--- ch03-playground.js
+++ ch03-playground.js
@@ -1,3 +1,5 @@
+// Function declaration scope
+
 let firstName = "Stas";
 
 printName();
@@ -11,3 +13,22 @@ function printName() {
 function getName() {
     return firstName;
 }
+
+// Closure examples.
+
+function wrapValue(n) {
+    return () => n++;
+}
+
+let wrap = wrapValue(2);
+
+for (let i = 0; i < 10; i++) {
+    console.log(wrap());
+}
+
+function multiplier(factor) {
+    return number => number * factor;
+}
+
+let twice = multiplier(2);
+console.log("Twice:", twice(7));
