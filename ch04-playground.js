fd1204f53ad1f0d42760aac808f8a3715352b65b
Stanislav Kalashnikov <halega@gmail.com>
2025-03-29 10:03:36 +0300

    ch08: bugs and errors

--- ch04-playground.js
+++ ch04-playground.js
@@ -1,3 +1,30 @@
+var fs = require("fs");
+
+fs.stat("ch04-playground.js", (error, stats) => {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(stats);
+		console.log("Path is file:", stats.isFile());
+		console.log("Path is directory:", stats.isDirectory());
+	}
+});
+
 let listOfNumbers = [2, 3, 5, 7, 11];
 console.log(listOfNumbers[0]);
 console.log(listOfNumbers[2]);
+
+function numberToString(n, base = 10) {
+	let result = "", sign = "";
+	if (n < 0) {
+		sign = "-";
+		n = -n;
+	}
+	do {
+		result = String(n % base) + result;
+		n = Math.floor(n / base);
+	} while (n > 0);
+	return sign + result;
+}
+
+console.log(numberToString(141));
